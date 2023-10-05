f8c16303474e665922d68184d07a69fb7ac46ba2
Caique <ribeiro.caique95@gmail.com>
2023-10-04 21:21:58 -0300

    config: adds structure to deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+module.exports = (phase) => {
+  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  // when `next build` or `npm run build` is used
+  const isProd = phase === PHASE_PRODUCTION_BUILD
+
+  const env = {
+    NEXT_PUBLIC_APP_URL: (() => {
+      if (isDev) return "http://localhost:3000";
+      if (isProd) return "http://localhost:3000";
+      return "NEXT_PUBLIC_AUTH_DOMAIN:not (isDev,isProd)";
+    })(),
+  }
+
+  const images = {
     domains: [
       'files.stripe.com'
     ]
   }
-}
 
-module.exports = nextConfig
+  async function headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  }
+
+  return {
+    env,
+    images,
+    headers
+  }
+}
