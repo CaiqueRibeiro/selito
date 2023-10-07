64023e127788e421a835786190cc1cf94a03d8cb
Caique <ribeiro.caique95@gmail.com>
2023-10-07 12:59:02 -0300

    feature: gets products in admin from API

--- src/app/(home)/admin/products/page.tsx
+++ src/app/(home)/admin/products/page.tsx
@@ -1,13 +1,52 @@
 'use client'
 import { useSession } from 'next-auth/react'
-import { RectangleCard } from '@/components/RetangleCard'
-import { Edit, Search, ShoppingBasket, ShoppingBasketIcon, X } from 'lucide-react'
-import Image from 'next/image'
+import { Search } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import ProductsTable from '@/components/ProductsTable'
 
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: string;
+}
 
+interface Category {
+  value: string
+  name: string
+}
 
 export default function ProductsList() {
   const { data: session } = useSession()
+  const [products, setProducts] = useState<Product[]>([])
+  const [categories, setCategories] = useState<Category[]>([
+    { value: '', name: 'All' },
+    { value: 'Highlights', name: 'Highlights' },
+    { value: 'Clothes', name: 'Clothes' }
+  ])
+  const [chosenCategory, setChosenCategory] = useState<string>('')
+
+  useEffect(() => {
+    async function getProduct() {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/products?category=${chosenCategory}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+
+      if (response.ok) {
+        const { products } = await response.json()
+        setProducts(products)
+      }
+    }
+
+    getProduct()
+  }, [chosenCategory])
+
+  function handleChooseCategory(element:any) {
+    setChosenCategory(element.target.value)
+  }
 
   return (
     <main className="flex-1 flex flex-col items-center justify-start gap-8 mt-8 px-8">
@@ -26,122 +65,14 @@ export default function ProductsList() {
             </span>
           </div>
 
-          <select id="countries" defaultChecked={false} className="group bg-violet-700 text-zinc-50 text-sm rounded-md w-1/5 p-3">
-            <option value="">All</option>
-            <option value="US">Highlights</option>
-            <option value="CA">Clothes</option>
-            <option value="FR">Mangás</option>
-            <option value="DE">Drugs</option>
+          <select id="countries" value={chosenCategory} onChange={handleChooseCategory} className="group bg-violet-700 text-zinc-50 text-sm rounded-md w-1/5 p-3">
+            {categories.map(category => (
+              <option value={category.value} key={category.name}>{category.name}</option>
+            ))}
           </select>
         </div>
 
-        <div className="mt-10">
-          <table className="w-full table-auto border-collapse text-white text-left text-xs">
-
-            <thead className="text-xs uppercase font-bold">
-              <tr className="">
-                <td className="px-4 py-2">Product</td>
-                <td className="px-4 py-2 text-center">Category</td>
-                <td className="px-4 py-2 text-center">Quantity</td>
-                <td className="px-4 py-2 text-center">Price</td>
-                <td className="px-4 py-2 w-24"></td>
-              </tr>
-            </thead>
-
-            <tbody className="[&>tr:hover]:bg-violet-500 [&>*:nth-child(odd)]:bg-violet-500/20 [&>*:nth-child(even)]:bg-violet-500/5">
-              <tr className="border-b border-white/20">
-                <td className="px-4 py-2 flex items-center gap-4">
-                  <Image
-                    className="rounded-full h-10 w-10 border-none"
-                    width={48}
-                    height={48}
-                    alt="User Image"
-                    src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  />
-                  <span>Alfreds Futterkiste</span>
-                </td>
-                <td className="px-4 py-2  text-center">HighLights</td>
-                <td className="px-4 py-2 text-center">184</td>
-                <td className="px-4 py-2  text-center">
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  }).format(50)}</td>
-                <td className="px-4 py-2">
-                  <div className="flex items-center justify-between gap-2">
-                    <div className="rounded-sm bg-violet-700 p-2 w-fit">
-                      <Edit size={12} />
-                    </div>
-                    <div className="rounded-sm bg-red-600 p-2 w-fit">
-                      <X size={12} />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-
-              <tr className="border-b border-white/20">
-                <td className="px-4 py-2 flex items-center gap-4">
-                  <Image
-                    className="rounded-full h-10 w-10 border-none"
-                    width={48}
-                    height={48}
-                    alt="User Image"
-                    src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  />
-                  <span>Alfreds Futterkiste</span>
-                </td>
-                <td className="px-4 py-2  text-center">HighLights</td>
-                <td className="px-4 py-2 text-center">184</td>
-                <td className="px-4 py-2  text-center">
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  }).format(50)}</td>
-                <td className="px-4 py-2">
-                  <div className="flex items-center justify-between gap-2">
-                    <div className="rounded-sm bg-violet-700 p-2 w-fit">
-                      <Edit size={12} />
-                    </div>
-                    <div className="rounded-sm bg-red-600 p-2 w-fit">
-                      <X size={12} />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-
-              <tr className="border-b border-white/20">
-                <td className="px-4 py-2 flex items-center gap-4">
-                  <Image
-                    className="rounded-full h-10 w-10 border-none"
-                    width={48}
-                    height={48}
-                    alt="User Image"
-                    src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                  />
-                  <span>Alfreds Futterkiste</span>
-                </td>
-                <td className="px-4 py-2  text-center">HighLights</td>
-                <td className="px-4 py-2 text-center">184</td>
-                <td className="px-4 py-2  text-center">
-                  {new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  }).format(50)}</td>
-                <td className="px-4 py-2">
-                  <div className="flex items-center justify-between gap-2">
-                    <div className="rounded-sm bg-violet-700 p-2 w-fit">
-                      <Edit size={12} />
-                    </div>
-                    <div className="rounded-sm bg-red-600 p-2 w-fit">
-                      <X size={12} />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-
-            </tbody>
-          </table>
-        </div>
+        <ProductsTable products={products} />
       </div>
     </main>
   )
